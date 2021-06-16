fa9845932d863dd47878ffbd4a3c22a3f0720ea3
DenJerk <DenJerk@DESKTOP-AQNA45P>
2021-06-17 00:02:19 +0300

    Добавьте файлы проекта.

---
+++ FinalFight/Pages/pListMaterials.xaml.cs
@@ -0,0 +1,36 @@
+﻿using FinalFight.Classes;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FinalFight.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для pListMaterials.xaml
+    /// </summary>
+    public partial class pListMaterials : Page
+    {
+        public ObservableCollection<Material> Materials;
+        public pListMaterials()
+        {
+
+            InitializeComponent();
+            AppData.entObj.Material.Load();
+            Materials = AppData.entObj.Material.Local;
+            lvMaterials.ItemsSource = Materials;
+        }
+    }
+}
---
+++ FinalFight/Pages/pStart.xaml.cs
@@ -0,0 +1,39 @@
+﻿using FinalFight.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FinalFight.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для pStart.xaml
+    /// </summary>
+    public partial class pStart : Page
+    {
+        public pStart()
+        {
+            InitializeComponent();
+        }
+
+        private void bLstMat_Click(object sender, RoutedEventArgs e)
+        {
+            FrameApp.frmObj.Navigate(new pListMaterials());
+        }
+
+        private void bMtrWork_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
